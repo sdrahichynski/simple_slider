81e5817ade0c9dcc72a9c81eee342dfd8695b334
GrayD <siarhei.drahichynski@gmail.com>
2018-11-26 18:20:56 +0300

    nav container

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -2,55 +2,16 @@
 
 import { tns } from "tiny-slider";
 
-console.log('slider')
-
-function nextSlide (slider) {
-    const track = slider.querySelector('.js_track');
-    const slides = track.querySelectorAll('.js_slide');
-    const currentIndex = +track.dataset.index || 0;
-    const nextIndex = currentIndex + 1 > slides.length - 1 ? slides.length - 1 : currentIndex + 1;
-
-    track.style.transform = `translateX(-${100 * nextIndex}%)`;
-    track.dataset.index = nextIndex;
-}
-
-function prevSlide (slider) {
-    const track = slider.querySelector('.js_track');
-    const currentIndex = track.dataset.index || 0;
-    const nextIndex = currentIndex - 1 < 0 ? 0 : currentIndex - 1;
-
-    track.style.transform = `translateX(-${100 * nextIndex}%)`;
-    track.dataset.index = nextIndex;
-}
-
-function jumpTo (slider, index = 0) {
-    const track = slider.querySelector('.js_track');
-    track.style.transform = `translateX(-${100 * index}%)`;
-    track.dataset.index = index;
-}
-
-
-document.addEventListener('click', (e) => {
-    const currentTarget = e.target;
-    console.log(currentTarget)
-
-    if (currentTarget.classList.contains('js_next')) {
-        nextSlide(currentTarget.closest('.js_slider'));
-    }
-
-    if (currentTarget.classList.contains('js_prev')) {
-        prevSlide(currentTarget.closest('.js_slider'));
-    }
-
-    if (currentTarget.classList.contains('js_dot')) {
-        jumpTo(currentTarget.closest('.js_slider'), currentTarget.dataset.index);
-    }
-})
-
-
-window.nextSlide = nextSlide
-
-
 const slider = tns({
-    container: '.js_tiny'
-})
+    container: '.js_tiny', 
+    navContainer: '.js_nav_track'
+});
+
+
+
+function fitNavContainer () {
+    const sliderInfo = slider.getInfo()
+    const navContainer = sliderInfo.navContainer;
+}
+
+console.log(slider.getInfo().navContainer)
