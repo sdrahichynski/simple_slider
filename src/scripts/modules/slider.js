1c51fa5f5ebdda79c325324e39e5fe943ca8abfc
GrayD <siarhei.drahichynski@gmail.com>
2018-11-27 18:10:06 +0300

    smart dots

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -2,6 +2,11 @@
 
 import { tns } from "tiny-slider";
 
+const visibleDotsCount = 5;
+const fixOnDot = Math.round(visibleDotsCount / 2);
+const dotStep  = 12;
+
+
 const slider = tns({
     container: '.js_tiny', 
     navContainer: '.js_nav_track'
@@ -9,9 +14,23 @@ const slider = tns({
 
 
 
-function fitNavContainer () {
-    const sliderInfo = slider.getInfo()
-    const navContainer = sliderInfo.navContainer;
+function fitNavContainer (info) {
+    const { index, navContainer, slideCount } = info;
+    const maxStepsOffset = slideCount - visibleDotsCount;
+    
+    let stepsOffset;
+
+    if (index <= fixOnDot) {
+        stepsOffset = 0;
+    }
+
+    if (index > fixOnDot) {
+        stepsOffset = Math.min(index - fixOnDot, maxStepsOffset);
+    }
+
+    navContainer.style.transform = `translateX(${ -stepsOffset * dotStep }px)`;
 }
 
-console.log(slider.getInfo().navContainer)
+slider.events.on('indexChanged', (e) => {
+    fitNavContainer(e);
+})
