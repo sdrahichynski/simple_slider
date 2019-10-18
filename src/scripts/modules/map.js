1fdb2b44ba386162fa28f9f78cbdbfbe994c288b
GrayD <siarhei.drahichynski@gmail.com>
2019-10-18 16:19:04 +0300

    new module

--- src/scripts/modules/map.js
+++ src/scripts/modules/map.js
@@ -1,41 +1,69 @@
 'use strict';
 
+const PIN        = './image/common/travel-services/icon-map-pin.svg';
+const PIN_ACTIVE = './image/common/travel-services/icon-map-pin-active.svg';
 
-const loadScript = (
-    {
-        apiKey,
-        locale,
-        callbackName = 'googleMapsCallback',
-        callback     = () => {},
-        node
+class HotelMarker extends window.google.maps.Marker {
+    constructor (options) {
+        const extendedOptions = { ...HotelMarker.commonMarkerOptions, ...options };
+        super(extendedOptions);
+        this.addEventListeners();
     }
-) => {
-    const gmScript = document.createElement('script');
 
-    window[callbackName] = () => {
-        callback();
+    static icons = {
+        common: PIN,
+        hover : PIN_ACTIVE,
+        active: PIN_ACTIVE,
     };
 
-    gmScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&language=${locale}&callback=${callbackName}`;
-    document.head.appendChild(gmScript);
+    static zIndexes = {
+        common: 10,
+        hover : 100,
+        active: 1000,
+    };
+
+    static commonMarkerOptions = {
+        icon  : HotelMarker.icons.common,
+        zIndex: HotelMarker.zIndexes.common,
+    };
+
+    setMarkerStyle = style => {
+        this.setIconStyle(style);
+        this.setZIndexStyle(style);
+    };
+
+    setZIndexStyle = style => this.setZIndex(HotelMarker.zIndexes[style] ?? HotelMarker.zIndexes.common);
+    setIconStyle   = style => this.setIcon(HotelMarker.icons[style] ?? HotelMarker.icons.common);
+
+    addEventListeners = () => {
+        this.addListener('mouseout',  () => this.setMarkerStyle('common'));
+        this.addListener('mouseover', () => this.setMarkerStyle('hover'));
+        // this.addListener('click',     () => this.setIconStyle('active'));
+    }
 }
 
-class GoogleMap {
-    constructor (props) {
-        const { node, options } = props;
-        this.options = { ...GoogleMap.commonOptions, options };
+class HotelsGoogleMap {
+    constructor ({ node, options }) {
+        this.options = { ...HotelsGoogleMap.commonOptions, ...options };
+        this.markers = {};
         this.initMap(node, this.options);
     }
 
     static commonOptions = {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    }
+        center: { lat: 55.75838088989258, lng: 37.62165069580078 },
+        zoom: 8,
+    };
 
     initMap = (node, options) => {
-        this.map = new window.google.maps.Map(node, options)
-    }
+        this.map = new window.google.maps.Map(node, options);
+    };
+
+    addMarkers = (markers = []) => markers.forEach(marker => this.addMarker(marker));
+
+    addMarker = ({ id, ...options }) => this.markers[id] = new HotelMarker({
+        map: this.map,
+        ...options,
+    });
 }
 
-
-export { loadScript, GoogleMap };
+export { HotelsGoogleMap };
